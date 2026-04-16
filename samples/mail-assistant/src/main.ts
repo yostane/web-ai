5841b9e3dadc37b01a98807abf24be362cd50c66
yostane <1958676+yostane@users.noreply.github.com>
2026-04-16 19:24:13 +0200

    refactor(mail-assistant): restructure HTML layout and enhance form styling; improve TypeScript logic for suggestions

--- samples/mail-assistant/src/main.ts
+++ samples/mail-assistant/src/main.ts
@@ -3,20 +3,23 @@ import { setupWriter } from "./utils";
 
 async function setupSuggestions() {
   const writer = await setupWriter();
-  console.log("Setup completed");
-
   document.getElementById("loading")?.remove();
   document.getElementById("mail-form")!.style.display = "block";
+  console.log("Setup completed");
+
   const titleInput =
     document.querySelector<HTMLInputElement>("input[name=title]")!;
   const contentInput = document.querySelector<HTMLTextAreaElement>(
     "textarea[name=content]",
   )!;
-  //const nextWordSuggestion = document.getElementById("next-word-suggestion")!;
+  const nextWordSuggestion = document.getElementById("next-word-suggestion")!;
 
   let stream: ReadableStream<string> | null = null;
+  let currrentSuggestion = "";
   contentInput.addEventListener("input", async () => {
     stream?.cancel();
+    currrentSuggestion = "";
+    nextWordSuggestion.textContent = "";
     const title = titleInput.value;
     const content = contentInput.value;
     stream = writer.writeStreaming(
@@ -24,6 +27,8 @@ async function setupSuggestions() {
     );
     for await (const chunk of stream) {
       console.log("Received chunk:", chunk);
+      currrentSuggestion += chunk;
+      nextWordSuggestion.textContent = currrentSuggestion;
     }
   });
 }
